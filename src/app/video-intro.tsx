548e76e552c5ade83100a5a1f685b997eeceb4fa
Shreyas B <shreyasbigbang@gmail.com>
2024-01-10 22:52:11 +0530

    video resolution fixes

--- src/app/video-intro.tsx
+++ src/app/video-intro.tsx
@@ -30,7 +30,7 @@ export function VideoIntro() {
           </Typography>
         </div>
           <div className="justify-self-center mb-10 lg:m-10 overflow-hidden rounded-xl relative mx-auto max-w-6xl ">
-            <video className="h-[60vh] w-auto rounded-lg" controls autoPlay loop>
+            <video className="h-[30rem] w-full aspect-square rounded-lg object-cover" controls autoPlay loop>
               <source
                 src="https://s3.eu-west-2.amazonaws.com/envy.public/media/video/2023-12-24_18_30_12_video.MP4"
                 type="video/mp4"
