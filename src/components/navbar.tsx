78218ee11520c3023c32057e2c0e635185beea37
santhosh-anbalagan <46365378+santhosh-anbalagan@users.noreply.github.com>
2024-01-12 00:27:34 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -68,7 +68,7 @@ export function Navbar() {
 
         <Link href="/">
           <Image
-            width={isScrolling?85:100}
+            width={85}
             height={200}
             src={isScrolling ? '/logos/envy-ico-green.svg' : '/logos/envy-ico-white.svg'}
             alt="team work"
